16ae77431b39095de4e64f3c4701f51c077cf4ee
quandd31112 <dragon35128290@gmail.com>
2025-06-25 08:06:49 +0700

    Force push: Replace GitHub repo with local project

--- app/src/main/java/com/example/infobook/CountriesActivity.java
+++ app/src/main/java/com/example/infobook/CountriesActivity.java
@@ -17,7 +17,23 @@ public class CountriesActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.viewPager);
 
         TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(CountryFragment.newInstance("United Kingdom info", R.drawable.flag_uk), "UK");
+        adapter.addFragment(CountryFragment.newInstance("United Kingdom infoAndroid Studio is the official IDE for Android development, and includes everything you need to build Android apps.\n" +
+                "\n" +
+                "This page lists new features and improvements in the latest version in the stable channel, Android Studio Narwhal. You can download it here or update to it inside Android Studio by clicking Help > Check for updates (Android Studio > Check for updates on macOS)\n" +
+                "\n" +
+                "This is a stable release of Android Studio. Note that patches might contain new minor features and bug fixes. See Android Studio release names to understand Android Studio version naming.\n" +
+                "\n" +
+                "To see what's been fixed in this version of Android Studio, see the closed issues.\n" +
+                "\n" +
+                "To view the release notes for older versions of Android Studio, see Past releases.\n" +
+                "\n" +
+                "For early access to upcoming features and improvements, see the Preview builds of Android Studio.\n" +
+                "\n" +
+                "If you encounter problems in Android Studio, check the Known issues or Troubleshoot page.\n" +
+                "\n" +
+                "\n" +
+                "Android Gradle plugin and Android Studio compatibility\n" +
+                "The Android Studio build system is based on Gradle, and the Android Gradle plugin (AGP) adds several features that are specific to building Android apps. The following table lists which version of AGP is required for each version of Android Studio.", R.drawable.flag_uk), "UK");
         adapter.addFragment(CountryFragment.newInstance("Italy info", R.drawable.flag_italy), "Italy");
         adapter.addFragment(CountryFragment.newInstance("France info", R.drawable.flag_france), "France");
 
